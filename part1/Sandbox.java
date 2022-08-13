bcb99bcd7a491725d9815da2cc6fdc1d63567c52
Snipacer <92214782+SNIPACER@users.noreply.github.com>
2022-08-13 10:11:51 +0530

    Completed
    Sandbox program

---
+++ part1/Sandbox.java
@@ -0,0 +1,10 @@
+public class Sandbox {
+  /*
+    first program on mooc
+  */
+
+    public static void main(String[] args) {
+        // Write your program here
+        System.out.println("Welcome to programming");
+    }
+}
