e040e00c2902827237c94d6f4e769e98364404e8
Snipacer <92214782+SNIPACER@users.noreply.github.com>
2022-08-13 12:27:59 +0530

    Create Dummy2.java

---
+++ part2/practice/Dummy2.java
@@ -0,0 +1,25 @@
+/*
+ What does the program below print?
+*/
+public class Dummy2{
+public static void main(String[] args) {
+    int number = 0;
+
+    while (true) {
+        number = number + 1;
+
+        if (number >= 5) {
+            break;
+        }
+
+        if (number < 5) {
+            continue;
+        }
+
+        System.out.print(number + " ");
+    }
+
+    System.out.print(number + " ");
+}
+  
+}
