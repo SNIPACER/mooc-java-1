933f18bd31565603bb74ede57a5d00aa6d3656a6
Snipacer <92214782+SNIPACER@users.noreply.github.com>
2022-08-13 11:20:07 +0530

    Create Adulthood.java

---
+++ part1/Adulthood.java
@@ -0,0 +1,31 @@
+/*
+Write a program that prompts the user for their age and tells them whether or not 
+they are an adult (18 years old or older).
+
+How old are you?
+12
+You are not an adult
+
+How old are you?
+32
+You are an adult
+
+
+*/
+import java.util.Scanner;
+ 
+public class Adulthood {
+ 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+ 
+        // Write your program here 
+        System.out.println("How old are you?");
+        int age=Integer.valueOf(scan.nextLine());
+        if(age>=18){
+            System.out.println("You are an adult");
+        }else{
+            System.out.println("You are not an adult");
+        }
+    }
+}
