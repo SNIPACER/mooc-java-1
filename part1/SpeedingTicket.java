68eac847effb86d4820eeb795a125838d2133e06
Snipacer <92214782+SNIPACER@users.noreply.github.com>
2022-08-13 11:01:44 +0530

    Create SpeedingTicket.java

---
+++ part1/SpeedingTicket.java
@@ -0,0 +1,30 @@
+/*
+Write a program that asks the user for an integer and prints the string 
+"Speeding ticket!" if the input is greater than 120.
+
+Give speed:
+15
+
+Give speed:
+135
+Speeding ticket!
+
+
+*/
+
+import java.util.Scanner;
+
+public class SpeedingTicket {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here. 
+        System.out.println("Give speed:");
+        int speed=Integer.valueOf(scanner.nextLine());
+        if(speed>120){
+            System.out.println("Speeding ticket!");
+        }
+    }
+}
+
