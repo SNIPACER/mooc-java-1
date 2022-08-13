5a62975d667a92086ee4bddab836fa4e45eea597
Snipacer <92214782+SNIPACER@users.noreply.github.com>
2022-08-13 12:08:05 +0530

    Create SquareRootOfSum.java

---
+++ part2/SquareRootOfSum.java
@@ -0,0 +1,34 @@
+/*
+Write a program that reads two integers from the user and prints the square root of 
+the sum of these integers. The program does not need to work with negative values.
+
+You can calculate the square root of an integer with the command Math.sqrt like this:
+int number = 42;
+double squareRoot = Math.sqrt(number);
+System.out.println(squareRoot);
+
+Here are a few examples:
+1
+0
+1
+
+5
+4
+3
+
+1
+35
+6
+*/
+import java.util.Scanner;
+ 
+public class Squared {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int value=Integer.valueOf(scanner.nextLine());
+        System.out.println(value*value);
+ 
+    }
+}
