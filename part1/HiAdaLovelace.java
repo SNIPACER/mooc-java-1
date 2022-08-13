2c996259b7cc1e6a434bd8d0dd70319940c4365f
Snipacer <92214782+SNIPACER@users.noreply.github.com>
2022-08-13 10:23:55 +0530

    Create HiAdaLovelace.java

---
+++ part1/HiAdaLovelace.java
@@ -0,0 +1,32 @@
+/*
+ Q)   The exercise template contains the following program.
+
+  public class HiAdaLovelace {
+
+      public static void main(String[] args) {
+          String name = "Ada Lovelace";
+
+      }
+  }
+  
+  Modify the program so that it prints the contents of the variable "name", and the printed text is the following in its full form:
+ 
+  Hi Ada Lovelace!
+  
+  NB! When using the System.out.println command,
+  do not pass in the string "Ada Lovelace" as a parameter.
+  Instead, use the existing variable name: System.out.println("Hi " + ...)
+*/
+
+public class HiAdaLovelace {
+
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+        System.out.println("Hi "+name+"!");
+
+
+    }
+}
+
+
+
