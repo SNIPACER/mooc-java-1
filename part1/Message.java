5173143fbecf52e4d4c2564f36fc8a8b626e769b
Snipacer <92214782+SNIPACER@users.noreply.github.com>
2022-08-13 10:20:13 +0530

    Create Message.java

---
+++ part1/Message.java
@@ -0,0 +1,39 @@
+/*
+Q) Write a program that asks the user to write a string. When the user has provided a string
+   (i.e., written some text and pressed the enter key), the program should print the string that was provided by the user.
+   The exercise template comes with a program template that includes the creation of a Scanner tool.
+
+import java.util.Scanner;
+
+public class Message {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message: ");
+        // Write your program here
+    }
+}
+
+OUtput:-
+--------
+Write a message:
+Bye
+Bye
+
+
+*/
+import java.util.Scanner;
+
+public class Message {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message:");
+        // Write your program here
+        String message=scanner.nextLine();
+        System.out.println(message);
+
+    }
+}
