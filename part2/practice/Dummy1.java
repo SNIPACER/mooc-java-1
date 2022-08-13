bc9bcd7e174e1acf65c89569e814017a439803e3
Snipacer <92214782+SNIPACER@users.noreply.github.com>
2022-08-13 12:24:49 +0530

    Update Dummy1.java

--- part2/practice/Dummy1.java
+++ part2/practice/Dummy1.java
@@ -1,1 +1,37 @@
-/**/
+/*
+DUmmy
+The output of the program can be as follows:
+--------------------------------------------
+Input a number, 0 to quit
+5
+You input 5
+Input a number, 0 to quit
+-2
+You input -2
+Input a number, 0 to quit
+0
+Done, thank you!
+*/
+
+import java.util.Scanner;
+
+public class PracticeDummy{
+  
+  public static void main(String[] args){
+  
+    Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Input a number, 0 to quit");
+             int command = Integer.valueOf(scanner.nextLine());
+             if (command == 0) {
+                break;
+             }
+
+             System.out.println("You input " + command);
+        }
+
+      System.out.println("Done, thank you!");
+  }
+  
+}
