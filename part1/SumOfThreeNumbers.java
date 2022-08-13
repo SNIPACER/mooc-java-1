846a6a1e484a3e928ad3f6ea3fef0cea0d558f94
Snipacer <92214782+SNIPACER@users.noreply.github.com>
2022-08-13 10:52:28 +0530

    Create SumOfThreeNumbers.java

---
+++ part1/SumOfThreeNumbers.java
@@ -0,0 +1,43 @@
+/*
+Write a program that asks the user for three numbers. After this the program prints the
+sum of the numbers given by the user.
+
+The program should work like this:
+--------------------------------------
+Give the first number:
+8
+Give the second number:
+3
+Give the third number:
+3
+The sum of the numbers is 14
+
+Give the first number:
+3
+Give the second number:
+-1
+Give the third number:
+2
+The sum of the numbers is 4
+
+*/
+
+
+import java.util.Scanner;
+
+public class SumOfThreeNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the third number:");
+        int third = Integer.valueOf(scanner.nextLine());
+        System.out.println("The sum of the numbers is "+(first+second+third));
+        
+    }
+}
